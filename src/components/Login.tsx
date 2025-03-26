f96db724d859de9c53df12671643a8dda4ca63a6
Renato <jrsolano90@gmail.com>
2025-03-26 17:34:43 -0500

    Carga 04 del 26-03

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,45 @@
-
 import React from "react";
 import "./../styles/Login.css"; // Importamos el archivo css
-import {useNavigate} from "react-router-dom"; // Importamos el hook useNavigate
+import { useNavigate } from "react-router-dom"; // Importamos el hook useNavigate
 
 const Login: React.FC = () => {
+  const navigate = useNavigate(); // Declaramos la variable navigate que contiene el hook useNavigate
 
-    const navigate = useNavigate(); // Declaramos la variable navigate que contiene el hook useNavigate 
-
-    return (
-        <div className="login-container">
-            <h1>Win - Login</h1>
-            <h2>Iniciar Sesión</h2>
-            <form className={"login-form"}>
-                <div className="input-container">
-                    <label>Usuario: </label>
-                    <input 
-                    type="text" placeholder="Ingrese su usuario"/>
-                </div>
-                <div className="input-container">
-                    <label>Contraseña: </label>
-                    <input 
-                    type="password" id="password" 
-                    placeholder="Ingrese su contraseña"/>
-                </div>
-                <button type="button" className="login-button" onClick={()=> navigate('/home')}>Iniciar Sesión</button>
-                <p className="forgot-password" onClick={()=> navigate('/recuperar-password')}>
-                    ¿Olvidaste tu contraseña?
-                </p>
-            </form>
-        </div>
-    );
+  return (
+    <div className="login">
+      <div className="login-container">
+        <h1>Win - Login</h1>
+        <h2>Iniciar Sesión</h2>
+        <form className={"login-form"}>
+          <div className="input-container">
+            <label>Usuario: </label>
+            <input type="text" placeholder="Ingrese su usuario" />
+          </div>
+          <div className="input-container">
+            <label>Contraseña: </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Ingrese su contraseña"
+            />
+          </div>
+          <button
+            type="button"
+            className="login-button"
+            onClick={() => navigate("/home")}
+          >
+            Iniciar Sesión
+          </button>
+          <p
+            className="forgot-password"
+            onClick={() => navigate("/recuperar-password")}
+          >
+            ¿Olvidaste tu contraseña?
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
