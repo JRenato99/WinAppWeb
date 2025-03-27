a31c1b50cf40985ab68b853b75ccbbd4cd2f29ee
Renato <jrsolano90@gmail.com>
2025-03-27 11:39:18 -0500

    Carga 02 del 27-03

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,57 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import logo from "../assets/logo.png";
 
 export const Navbar: React.FC = () => {
+  const [isAdminOpen, setIsAdminOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      {/* Logo */}
-      <div className="navbar-logo">
-        <img src={logo} alt="Win" />
-      </div>
+      <div className="navbar-container">
+        {/* Logo */}
+        <Link to="/home" className="navbar-logo">
+          <img src={logo} alt="Win" />
+        </Link>
 
-      {/* Menú */}
-      <ul className="navbar-menu">
-        <li>
-          <Link to="/home/overview">Resumen</Link>
-        </li>
-        <li>
-          <Link to="/home/devices">Dispositivos</Link>
-        </li>
-        <li>
-          <Link to="/home/faults">Fallos</Link>
-        </li>
+        {/* Botón de menú para pantallas pequeñas */}
+        <button
+          className="navbar-toggle"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          ☰
+        </button>
 
-        {/* Admin */}
-        <li className="dropdown">
-          <span>Admin ▼</span>
-          <ul className="dropdown-menu">
-            <li>
-              <Link to="/home/admin/presets">Preajustes</Link>
-            </li>
-            <li>
-              <Link to="/home/admin/provisions">Disposiciones</Link>
-            </li>
-            <li>
-              <Link to="/home/admin/files">Archivos</Link>
-            </li>
-            <li>
-              <Link to="/home/admin/config">Configuracion</Link>
-            </li>
-            <li>
-              <Link to="/home/admin/permissions">Permisos</Link>
-            </li>
-            <li>
-              <Link to="/home/admin/users">Usuarios</Link>
-            </li>
-          </ul>
-        </li>
-      </ul>
+        {/* Menú */}
+        <ul className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
+          <li>
+            <Link to="/home/overview">Resumen</Link>
+          </li>
+          <li>
+            <Link to="/home/devices">Dispositivos</Link>
+          </li>
+          <li>
+            <Link to="/home/faults">Fallos</Link>
+          </li>
 
-      {/* Botón de Cerrar Sesión */}
-      <div className="navbar-logout">
-        <Link to="/">Cerrar Sesión</Link>
+          {/* Admin con hover */}
+
+          <li
+            className="admin-menu"
+            onMouseEnter={() => setIsAdminOpen(true)}
+            onMouseLeave={() => setIsAdminOpen(false)}
+          >
+            <span>Admin ▼</span>
+            {isAdminOpen && (
+              <ul className="dropdown">
+                <li>
+                  <Link to="/home/admin/presets">Preajustes</Link>
+                </li>
+                <li>
+                  <Link to="/home/admin/provisions">Disposiciones</Link>
+                </li>
+                <li>
+                  <Link to="/home/admin/files">Archivos</Link>
+                </li>
+                <li>
+                  <Link to="/home/admin/config">Configuracion</Link>
+                </li>
+                <li>
+                  <Link to="/home/admin/permissions">Permisos</Link>
+                </li>
+                <li>
+                  <Link to="/home/admin/users">Usuarios</Link>
+                </li>
+              </ul>
+            )}
+          </li>
+          {/* Botón de Cerrar Sesión */}
+          <li className="logout">
+            <Link to="/">Cerrar Sesión</Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
